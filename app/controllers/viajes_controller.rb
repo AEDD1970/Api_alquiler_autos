04df035122a01b7a7d58e3d71fb4df13ac89de1a
alexisduque12 <alexis.duque@hrinteractive>
2020-01-26 22:04:44 -0500

    ajuste controller viaje register

--- app/controllers/viajes_controller.rb
+++ app/controllers/viajes_controller.rb
@@ -27,11 +27,10 @@ class ViajesController < ApplicationController
   # POST /viajes.json
 
   def create_viaje
-    @viaje = Viaje.new(viaje_params)
+    #@viaje = Viaje.new(viaje_params)
     #
-    if  @viaje.save
-     render json:  @viaje, status: :created, location:  @viaje, notice: 'Propietario was successfully create.'
-     end
+    #if  @viaje.save
+    #  render json:  @viaje, status: :created, location:  @viaje, notice: 'Propietario was successfully create.'
     #else
     #  render json:  @viaje.errors, status: :unprocessable_entity
     #end
